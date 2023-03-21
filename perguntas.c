543bddbf09bf7ad9712e00dfe12116c84ba56d49
daani125 <128492691+daani125@users.noreply.github.com>
2023-03-21 12:40:54 -0300

    Create perguntas.c

---
+++ perguntas.c
@@ -0,0 +1,306 @@
+#include <stdio.h>
+int main()
+{
+	int acertos, erros, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10;
+	
+	erros=0;
+	acertos=0;
+
+	
+	printf("PERGUNTA 1: \n");
+	printf("Quando foi criado o primeiro computador?\n\n");
+	printf("Alternativas: \n");
+	printf("1.(A).1943 e 1946       2.(B)2008 e 2011       3.(C)1762 e 1765\n\n");
+	scanf("%d",&n1);
+	
+	if (n1==1)
+	{
+		acertos= acertos+1;
+		printf("Resposta Correta! \n\n\n");
+	}
+
+
+	else
+	{
+    	erros=erros+1;
+    	printf("Resposta Errada! \n\n\n");
+	}
+	
+	printf("*************************************** \n\n");
+	printf("Total de acertos: %d\n", acertos);
+	printf("Total de erros: %d\n\n", erros);
+	
+	
+	
+	
+	
+	printf("*************************************** \n\n\n\n\n");
+	
+	printf("PERGUNTA 2: \n");
+	printf("Em que pais foi criado o primeiro computador?\n\n");
+	printf("Alternativas: \n");
+	printf("1.(A)Russia       2.(B)EUA      3.(C)Brasil e 1765\n\n");
+	scanf("%d",&n2);
+	
+   if (n2==2)
+	{
+		acertos= acertos+1;
+		printf("Resposta Correta! \n\n\n");
+	}
+
+
+	else
+	{
+    	erros=erros+1;
+    	printf("Resposta Errada! \n\n\n");
+	}
+	
+	printf("*************************************** \n\n");
+	printf("Total de acertos: %d\n", acertos);
+	printf("Total de erros: %d\n\n", erros);
+	
+	
+	
+	
+	
+		printf("*************************************** \n\n\n\n\n");
+	
+	printf("PERGUNTA 3: \n");
+	printf("Qual tag e utilizada para adicionar imagens no html?\n\n");
+	printf("Alternativas: \n");
+	printf("1.(A)<img>       2.(B)<gif>      3.(C)<add_image>\n\n");
+	scanf("%d",&n3);
+	
+   if (n3==1)
+	{
+		acertos= acertos+1;
+		printf("Resposta Correta! \n\n\n");
+	}
+
+
+	else
+	{
+    	erros=erros+1;
+    	printf("Resposta Errada! \n\n\n");
+	}
+	
+	printf("*************************************** \n\n");
+	printf("Total de acertos: %d\n", acertos);
+	printf("Total de erros: %d\n\n", erros);
+	
+	
+	
+	
+	
+		printf("*************************************** \n\n\n\n\n");
+	
+	printf("PERGUNTA 4: \n");
+	printf("Como fechamos uma tag no html?\n\n");
+	printf("Alternativas: \n");
+	printf("1.(A)Utilizamos /. Exemplo </title>      2.(B)Utilizamos . Exemplo<.title>      3.(C)Utilizamos ! Exemplo <!title>\n\n");
+	scanf("%d",&n4);
+	
+   if (n4==1)
+	{
+		acertos= acertos+1;
+		printf("Resposta Correta! \n\n\n");
+	}
+
+
+	else
+	{
+    	erros=erros+1;
+    	printf("Resposta Errada! \n\n\n");
+	}
+	
+	printf("*************************************** \n\n");
+	printf("Total de acertos: %d\n", acertos);
+	printf("Total de erros: %d\n\n", erros);
+	
+	
+	
+	
+		
+		printf("*************************************** \n\n\n\n\n");
+	
+	printf("PERGUNTA 5: \n");
+	printf("Para que serve o background-color?\n\n");
+	printf("Alternativas: \n");
+	printf("1.(A)Definir a cor da letra      2.(B)Definir a fonte de uma letra      3.(C)Definir a cor de fundo da pagina\n\n");
+	scanf("%d",&n5);
+	
+   if (n5==3)
+	{
+		acertos= acertos+1;
+		printf("Resposta Correta! \n\n\n");
+	}
+
+
+	else
+	{
+    	erros=erros+1;
+    	printf("Resposta Errada! \n\n\n");
+	}
+	
+		printf("*************************************** \n\n");
+	printf("Total de acertos: %d\n", acertos);
+	printf("Total de erros: %d\n\n", erros);
+	
+	
+	
+	
+	
+			printf("*************************************** \n\n\n\n\n");
+	
+	printf("PERGUNTA 6: \n");
+	printf("Para que serve o CSS?\n\n");
+	printf("Alternativas: \n");
+	printf("1.(A)Ajuda a melhorar os graficos de um jogo      2.(B)Ajuda na linguagem C      3.(C)Ajuda a padronizar as paginas\n\n");
+	scanf("%d",&n6);
+	
+   if (n6==3)
+	{
+		acertos= acertos+1;
+		printf("Resposta Correta! \n\n\n");
+	}
+
+
+	else
+	{
+    	erros=erros+1;
+    	printf("Resposta Errada! \n\n\n");
+	}
+	
+	printf("*************************************** \n\n");
+	printf("Total de acertos: %d\n", acertos);
+	printf("Total de erros: %d\n\n", erros);
+		
+	
+	
+	
+	
+			printf("*************************************** \n\n\n\n\n");
+	
+	printf("PERGUNTA 7: \n");
+	printf("Qual a linguagem utilizada para fazer um site?\n\n");
+	printf("Alternativas: \n");
+	printf("1.(A)Linguagem C      2.(B)HTML      3.(C)Portugol\n\n");
+	scanf("%d",&n7);
+	
+   if (n7==2)
+	{
+		acertos= acertos+1;
+		printf("Resposta Correta! \n\n\n");
+	}
+
+
+	else
+	{
+    	erros=erros+1;
+    	printf("Resposta Errada! \n\n\n");
+	}
+	
+	printf("*************************************** \n\n");
+	printf("Total de acertos: %d\n", acertos);
+	printf("Total de erros: %d\n\n", erros);
+			
+	
+	
+	
+	
+			printf("*************************************** \n\n\n\n\n");
+	
+	printf("PERGUNTA 8: \n");
+	printf("Qual das opcoes nao e uma linguagem de programacao \n\n");
+	printf("Alternativas: \n");
+	printf("1.(A)C      2.(B)C++      3.(C)Doircio\n\n");
+	scanf("%d",&n8);
+	
+   if (n8==3)
+	{
+		acertos= acertos+1;
+		printf("Resposta Correta! \n\n\n");
+	}
+
+
+	else
+	{
+    	erros=erros+1;
+    	printf("Resposta Errada! \n\n\n");
+	}
+	
+	printf("*************************************** \n\n");
+	printf("Total de acertos: %d\n", acertos);
+	printf("Total de erros: %d\n\n", erros);
+	
+	
+	
+	
+				printf("*************************************** \n\n\n\n\n");
+	
+	printf("PERGUNTA 9: \n");
+	printf("Qual tag e utilizada para dar quebra de linha em html? \n\n");
+	printf("Alternativas: \n");
+	printf("1.(A)</br>      2.(B)<jump>      3.(C)<p>\n\n");
+	scanf("%d",&n9);
+	
+   if (n9==1)
+	{
+		acertos= acertos+1;
+		printf("Resposta Correta! \n\n\n");
+	}
+
+
+	else
+	{
+    	erros=erros+1;
+    	printf("Resposta Errada! \n\n\n");
+	}
+	
+	printf("*************************************** \n\n");
+	printf("Total de acertos: %d\n", acertos);
+	printf("Total de erros: %d\n\n", erros);
+	
+	
+	
+	
+					printf("*************************************** \n\n\n\n\n");
+	
+	printf("PERGUNTA 10: \n");
+	printf("Qual o significado de Class em português? \n\n");
+	printf("Alternativas: \n");
+	printf("1.(A)Classe    2.(B)Impressão      3.(C)Entrada\n\n");
+	scanf("%d",&n10);
+	
+   if (n10==1)
+	{
+		acertos= acertos+1;
+		printf("Resposta Correta! \n\n\n");
+	}
+
+
+	else
+	{
+    	erros=erros+1;
+    	printf("Resposta Errada! \n\n\n");
+	}
+	
+	printf("*************************************** \n\n");
+	printf("Total de acertos: %d\n", acertos);
+	printf("Total de erros: %d\n\n", erros);
+	printf("++++++++++++++++++++++++++++++++++++++++\n\n");
+	
+	if(acertos==10)
+	{
+		printf("--------Parabens, voce acertou todas das questoes!!--------\n\n\n");
+	}
+	
+	else
+	{
+		printf("Voce acertou %d \n\n\n", acertos);
+		printf("Voce errou %d \n\n\n", erros);
+	}
+	printf("*************************************** \n\n");	
+	
+	return 0;
+}
